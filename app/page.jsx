a830724eefc2c9b70bf30acd9a9d44a4155f771b
Rony-Mathew <ronymathewareekkattu@gmail.com>
2023-07-30 00:45:57 +0530

    First Commit

---
+++ app/page.jsx
@@ -0,0 +1,19 @@
+import Feed from '@Components/Feed';
+
+const Home = () => {
+  return (
+    <section className="w-full flex-center flex-col">
+        <h1 className='head_text text-center'>
+            <span className='orange_gradient text-center'>Home for AI Prompts</span>
+        </h1>
+        <p className='desc text-center'>
+            Promptbook is a creative space where you can discover, create and share AI prompts.
+        </p>
+
+        <Feed />
+
+    </section>
+  )
+}
+
+export default Home
